2e47139fdb863360173883c6d2349f78094a9813
tdupuy <tom.dupuy@4sh.fr>
2025-07-22 12:17:41 +0200

    feat (ui) : init UI application

--- apps/ui/src/app/app.config.ts
+++ apps/ui/src/app/app.config.ts
@@ -1,13 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {routes} from './app.routes';
+import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {apiBaseUrlInterceptor} from './interceptor/api-base-url.interceptor';
+import {authInterceptor} from './interceptor/auth.interceptor';
+import { bearerTokenInterceptor } from './interceptor/bearer-token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideHttpClient(withInterceptors([apiBaseUrlInterceptor, bearerTokenInterceptor])),
+    provideHttpClient(withInterceptors([apiBaseUrlInterceptor, authInterceptor])),
   ]
 };
--- apps/ui/src/app/app.routes.ts
+++ apps/ui/src/app/app.routes.ts
@@ -1,3 +1,66 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {App} from './pages/app/app';
+import {Dashboard} from './pages/dashboard/dashboard';
+import {AuthGuard} from './guards/auth-guard';
+import {Login} from './pages/auth/login/login';
+import {Signup} from './pages/auth/signup/signup';
+import {ForgotPassword} from './pages/auth/forgot-password/forgot-password';
+import {ResetPassword} from './pages/auth/reset-password/reset-password';
+import {NoAuthGuard} from './guards/no-auth-guard';
+import {AuthApp} from './pages/auth/auth-app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'auth',
+    component: AuthApp,
+    canActivate: [NoAuthGuard],
+    children: [
+      {
+        path: 'login',
+        component: Login,
+      },
+      {
+        path: 'signup',
+        component: Signup,
+      },
+      {
+        path: 'forgot-password',
+        component: ForgotPassword,
+      },
+      {
+        path: 'reset-password/:id',
+        component: ResetPassword,
+      },
+
+    ]
+  },
+  {
+    path: '',
+    component: App,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'dashboard',
+        component: Dashboard,
+      },
+      {
+        path: 'user',
+        component: Dashboard,
+      },
+      {
+        path: 'animal',
+        component: Dashboard,
+      },
+      {
+        path: 'booking',
+        component: Dashboard,
+      }
+    ]
+  },
+  {
+    path: "",
+    redirectTo: "auth/login",
+    pathMatch: "full"
+  }
+];
