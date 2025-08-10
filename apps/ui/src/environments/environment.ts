4bccfd5e33f5ca74bbd2425313c88ffdcb0be961
tdupuy <tom.dupuy@4sh.fr>
2025-08-10 15:06:53 +0200

    fix (ui) : set url to deployed api for prod env

--- apps/ui/src/environments/environment.ts
+++ apps/ui/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   environment : "production",
-  apiUrl : "",
+  apiUrl : "https://caniparadis.onrender.com",
   sentryDsn : "https://f778640a1af454ca4199701c7d0e8186@o4509780278706176.ingest.de.sentry.io/4509780460306512",
 };
